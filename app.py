7b88c159f501f7e238b97dba4ac8d1bbb0ecbdb8
Ciracino <didple81@gmail.com>
2024-09-19 22:26:55 +0900

    게시물 페이지 및 자동 줄바꿈 기능 추가

--- app.py
+++ app.py
@@ -38,6 +38,22 @@ def post():
 
     return render_template('post.html')
 
+@app.route('/post/<int:post_id>')
+def post_detail(post_id):
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    # sqlite3 에서는 SQL 쿼리의 파라미터를 바인딩할 때 튜플로 받음.
+    post = cur.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    conn.close()
+
+    if post:
+        return render_template('post_detail.html', post=post)
+    else:
+        return "Post not found", 404
+
+
 @app.route('/calculator', methods=['GET', 'POST'])
 def calculate():
     # post 이전 : 페이지 초기값
