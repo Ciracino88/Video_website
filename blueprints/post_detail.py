6100dec9a08540c9dd1f5b7bbabfaeadbd41a7a9
Ciracino <didple81@gmail.com>
2024-09-23 10:03:38 +0900

    댓글 기능 구현 완료

--- blueprints/post_detail.py
+++ blueprints/post_detail.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, session, redirect, url_for
 import sqlite3
+
 
 post_detail_bp = Blueprint('post_detail', __name__)
 
@@ -8,12 +9,28 @@ def post_detail(post_id):
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     cur = conn.cursor()
-
     # sqlite3 에서는 SQL 쿼리의 파라미터를 바인딩할 때 튜플로 받음.
     post = cur.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
+    comments = cur.execute('SELECT * FROM comments WHERE post_id = ?', (post_id,)).fetchall()
     conn.close()
 
+    # 게시물 데이터가 존재하면
     if post:
-        return render_template('post_detail.html', post=post)
+        return render_template('post_detail.html', post=post, comments=comments)
     else:
         return "Post not found", 404
+
+@post_detail_bp.route('/post/<int:post_id>/comment', methods=['POST'])
+def comment(post_id):
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
+    if request.method == 'POST':
+        cur.execute('''INSERT INTO comments (post_id, content, name)
+        VALUES (?, ?, ?)
+        ''', (post_id, request.form['comment'], session['username']))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for("post_detail.post_detail", post_id=post_id))
